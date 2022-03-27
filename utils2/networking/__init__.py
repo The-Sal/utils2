84bada87738cdd346fad6cf022acd3c5650c9fa6
The-Sal <70804343+The-Sal@users.noreply.github.com>
2022-03-27 09:22:56 +0400

    ---

--- utils2/networking/__init__.py
+++ utils2/networking/__init__.py
@@ -2,18 +2,30 @@ import requests as _r
 from utils2.networking._context import PrePostFunction
 from utils2.networking.downloadAdapter import adapter as _adapter_dl
 
+
 class Session:
-    def __init__(self, funcs=None, headers=None):
-        """
-            :param funcs A dictionary with 2 keys 'post' and 'pre' which contain 2 functions
-            :param headers The constant headers to send with all requests unless specified otherwise
+    def __init__(self, pre=None, post=None, headers=None):
+        """A session is a wrapper around the requests.
+
+        :param headers: The constant headers to send with all requests unless specified otherwise
+        :param pre: A function to run before the request, will receive the URL as a parameter
+        :param post: A function to run after the request, will receive the request object as a parameter
         """
 
-        self.session = _r.session()
-        self._preFunc = funcs['pre']
-        self._postFunc = funcs['post']
+
+        self._pre = pre
+        self._post = post
+
+        self._session = _r.session()
         self.headers = headers
 
+    def _runPre(self, value):
+        if self._pre is not None:
+            self._pre(value)
+
+    def _runPost(self, value):
+        if self._post is not None:
+            self._post(value)
 
 
     def get(self, url, headers=None, *args, **kwargs):
@@ -21,12 +33,10 @@ class Session:
         if headers is None:
             headers = self.headers
 
-        p = PrePostFunction(self._preFunc, self.post)
-        p.url = url
 
-        with p:
-            response = _r.get(url, headers=headers, *args, **kwargs)
-            p.request = response
+        self._runPre(url)
+        response = _r.get(url, headers=headers, *args, **kwargs)
+        self._runPost(response)
 
         return response
 
@@ -34,23 +44,14 @@ class Session:
         if headers is None:
             headers = self.headers
 
-        p = PrePostFunction(self._preFunc, self.post)
-        p.url = url
-
-        with p:
-            response = _r.post(url, data=data, json=json, headers=headers, *args, **kwargs)
-            p.request = response
+        self._runPre(url)
+        response = _r.post(url, data=data, json=json, headers=headers, *args, **kwargs)
+        self._runPost(response)
 
         return response
 
     def changeHeaders(self, headers):
         self.headers = headers
-
-    def changePreFunc(self, func):
-        self._preFunc = func
-
-    def changePostFunc(self, func):
-        self._postFunc = func
 
     def injectCookie(self, name, value):
         self.session.cookies.set(name=name, value=value)
@@ -62,3 +63,8 @@ class Session:
     @property
     def downloadAdapter(self):
         return _adapter_dl
+
+    @property
+    def session(self):
+        """The requests session"""
+        return self._session
--- utils2/networking/_context.py
+++ utils2/networking/_context.py
@@ -1,14 +1,14 @@
 class PrePostFunction:
     def __init__(self, pre, post):
-        self.pre = pre
-        self.post = post
+        self._pre = pre
+        self._post = post
         self.url = None
         self.request = None
 
     def __enter__(self):
-        if self.pre is not None:
-            self.pre(self.url)
+        if self._pre is not None:
+            self._pre(self.url)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        if self.post is not None:
-            self.post(self.request)
+        if self._post is not None:
+            self._post(self.request)
